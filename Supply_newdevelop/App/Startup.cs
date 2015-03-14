efaabeed3cfcb2e5eeca6030dcca2241c0743b25
Amin <aminsheikhi@gmail.com>
2015-03-14 15:55:43 +0330

    First on develop

---
+++ Supply_newdevelop/App/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(App.Startup))]
+
+namespace App
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            //Ioc.Register();
+            //config.DependencyResolver = new UnityResolver(DependencyManager.Container);
+
+            app.UseWebApi(config);
+            app.UseWelcomePage("/Index");
+        }
+    }
+}
